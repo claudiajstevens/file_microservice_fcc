b398a02de0ec4a3e259791d46ae6a3cf98b3d4f2
Claudia Stevens <71283725+claudiajstevens@users.noreply.github.com>
2022-09-06 01:26:55 +0000

    Created post request to upload a file.

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+//added
+const bodyParser = require('body-parser');
+var multer = require('multer');
+const upload = multer({dest:'uploads/'});
+
 var app = express();
 
 app.use(cors());
@@ -11,7 +16,22 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.use(bodyParser.urlencoded({extended: true}));
 
+//You can submit a form that includes a file upload.
+//The form file input field has the name attribute set to upfile.
+//When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+  }catch(err) {
+    res.send(400);
+  }
+});
 
 
 const port = process.env.PORT || 3000;
